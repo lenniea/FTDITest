f887b4e7ce4601390b85cf65ffd904e83a823b06
lenniea <lennie_araki@hotmail.com>
2017-07-17 15:09:38 -0700

    Revert to Developer Studio Resources (minus ResizeDialog.h & controls)

--- resource.h
+++ resource.h
@@ -1,24 +1,36 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by FTDITest.rc
+//
+#define IDD_MAIN                        100
+#define IDC_LISTVIEW                    1000
+#define IDC_DATA                        1001
+#define IDC_SEND                        1002
+#define IDC_PORT                        1003
+#define IDC_OPEN_CLOSE                  1004
+#define IDC_CLEAR                       1005
+#define IDC_COUNT                       1006
+#define IDC_REPEAT                      1007
+#define IDC_SET_BAUD                    1008
+#define IDC_BAUD_RATE                   1009
+#define IDC_LOG                         1010
+#define IDC_PROGRESS                    1011
+#define IDC_RTS                         1012
+#define IDC_DTR                         1013
+#define IDS_APP_NAME                    40000
+#define IDS_OPEN                        40001
+#define IDS_CLOSE                       40002
+#define IDS_LIST_HEADERS                40003
+#define IDS_ERR_DRAWITEM                40004
+#define IDC_STATIC                      -1
 
-#define IDD_MAIN                                100
-#define IDC_LISTVIEW                            1000
-#define IDC_DATA                                1001
-#define IDC_SEND                                1002
-#define IDC_PORT                                1003
-#define IDC_OPEN_CLOSE                          1004
-#define IDC_CLEAR                               1005
-#define IDC_COUNT                               1006
-#define IDC_REPEAT                              1007
-#define IDC_SET_BAUD                            1008
-#define IDC_BAUD_RATE                           1009
-#define IDC_LOG                                 1010
-#define IDC_PROGRESS                            1011
-#define IDC_RTS                                 1012
-#define IDC_DTR                                 1013
-#define IDS_APP_NAME                            40000
-#define IDS_OPEN                                40001
-#define IDS_CLOSE                               40002
-#define IDS_LIST_HEADERS                        40003
-#define IDS_ERR_DRAWITEM                        40004
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
